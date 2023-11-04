89853ad576664ddcd12f1b26df11ed7a1c2a4648
insolor <insolor@gmail.com>
2023-11-04 21:43:53 +0300

    Add code from df-translation-client

---
+++ csv_bisect_gui/bisect_tool.py
@@ -0,0 +1,183 @@
+import tkinter as tk
+from functools import partial
+from itertools import islice
+from operator import itemgetter
+from tkinter import ttk
+from typing import Any, Iterable
+
+from bidict import MutableBidict, bidict
+from tkinter_layout_helpers import grid_manager, pack_manager
+
+from csv_bisect_gui.scrollbar_frame import ScrollbarFrame
+
+
+class Node:
+    _all_items: list[Any]
+    start: int
+    end: int
+
+    def __init__(self, items: list, start: int = 0, end: int = None):
+        self._all_items = items
+        self.start = start
+
+        if end is None:
+            end = len(items) - 1
+
+        self.end = end
+        assert 0 <= self.start and self.end < len(items)
+
+    @property
+    def size(self):
+        return self.end - self.start + 1
+
+    def split(self) -> tuple["Node", "Node"]:
+        assert self.size >= 2, f"Not enough items to split: {self.size}"
+        mid = (self.start + self.end) // 2
+        return Node(self._all_items, self.start, mid), Node(self._all_items, mid + 1, self.end)
+
+    @property
+    def tree_text(self):
+        if self.size == 0:
+            return "[] (0 strings)"
+        elif self.size == 1:
+            return f"[{self.start} : {self.end}] (1 string)"
+        else:
+            return f"[{self.start} : {self.end}] ({self.size} strings)"
+
+    @property
+    def items(self) -> Iterable[tuple[str, str]]:
+        return islice(self._all_items, self.start, self.end + 1)
+
+    @property
+    def column_text(self) -> str:
+        if self.start > self.end:
+            return "<empty>"
+        elif self.start == self.end:
+            return repr(self._all_items[self.start])
+        else:
+            if self.end - self.start + 1 <= 2:  # One or two strings in the slice: show all strings
+                return ",".join(map(repr, self.items))
+            else:  # More strings: show the first and the last
+                return f"{self._all_items[self.start]!r} ... {self._all_items[self.end]!r}"
+
+    def __hash__(self):
+        return hash((self.start, self.end))
+
+    def __eq__(self, other: "Node"):
+        return self.start == other.start and self.end == other.end
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(..., {self.start}, {self.end})"
+
+
+class BisectTool(tk.Frame):
+    _strings: list[tuple[str, str]] | None
+    _nodes_by_item_ids: MutableBidict[str, Node]
+
+    def __init__(self, *args, strings: list[tuple[str, str]] | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        with grid_manager(self, sticky=tk.NSEW, pady=2) as grid:
+            scrollbar_frame = ScrollbarFrame(widget_factory=ttk.Treeview, show_scrollbars=tk.VERTICAL)
+
+            self.tree = tree = scrollbar_frame.widget
+            tree["columns"] = ("strings",)
+            tree.heading("#0", text="Tree")
+            tree.heading("#1", text="Strings")
+
+            self._nodes_by_item_ids = bidict()
+            self.strings = strings
+
+            grid.new_row().add(scrollbar_frame).configure(weight=1)
+
+            with pack_manager(tk.Frame(), side=tk.LEFT, expand=True, fill=tk.X, padx=1) as toolbar:
+                toolbar.pack_all(
+                    ttk.Button(text="Split", command=self.split_selected_node),
+                    ttk.Button(text="Mark as bad", command=partial(self.mark_selected_node, background="orange")),
+                    ttk.Button(text="Mark as good", command=partial(self.mark_selected_node, background="lightgreen")),
+                    ttk.Button(text="Clear mark", command=partial(self.mark_selected_node, background="white")),
+                )
+
+                grid.new_row().add(toolbar.parent)
+
+            grid.columnconfigure(0, weight=1)
+
+    @property
+    def strings(self) -> list[tuple[str, str]] | None:
+        return self._strings
+
+    @strings.setter
+    def strings(self, value):
+        self._strings = value
+        self.tree.delete(*self.tree.get_children())
+        self._nodes_by_item_ids = bidict()  # Create new empty bidict to avoid ValueDuplicationError
+        if value:
+            self.insert_node(Node(value))
+
+    def insert_node(self, node: Node, parent_node: Node | None = None):
+        if not parent_node:
+            parent_item_id = ""
+        else:
+            parent_item_id = self._nodes_by_item_ids.inverse[parent_node]
+
+        item_id = self.tree.insert(
+            parent_item_id,
+            tk.END,
+            text=node.tree_text,
+            values=(node.column_text,),
+            open=True,
+        )
+
+        self._nodes_by_item_ids[item_id] = node
+
+        # Add an item id as a tag to color the row by that tag
+        self.tree.item(item_id, tags=(item_id,))
+
+    def get_item_id_of_node(self, node: Node):
+        return self._nodes_by_item_ids.inverse[node]
+
+    def get_selected_node(self) -> Node | None:
+        tree = self.tree
+        selected_ids = tree.selection()
+        if selected_ids and not tree.get_children(selected_ids[0]):
+            return self._nodes_by_item_ids[selected_ids[0]]
+
+    def split_selected_node(self):
+        parent = self.get_selected_node()
+        if parent and parent.start != parent.end:
+            new_nodes = parent.split()
+
+            for node in new_nodes:
+                self.insert_node(parent_node=parent, node=node)
+
+            # move selection to the first child
+            item_id = self._nodes_by_item_ids.inverse[new_nodes[0]]
+            self.tree.selection_set(item_id)
+
+    def mark_selected_node(self, **kwargs):
+        tree = self.tree
+        for item in tree.selection():
+            tree.tag_configure(item, **kwargs)
+
+    @property
+    def selected_nodes(self) -> Iterable[Node]:
+        return (self._nodes_by_item_ids[item_id] for item_id in self.tree.selection())
+
+    @property
+    def filtered_strings(self):
+        nodes: list[Node] = list(self.selected_nodes)
+        if not nodes:
+            return self._strings
+        else:
+            if len(nodes) == 1:
+                # Only one row selected (optimized case)
+                return islice(self._strings, nodes[0].start, nodes[0].end + 1)
+            else:
+                # Merge ranges when multiple rows selected
+                enumerated_strings = list(enumerate(self._strings))
+                strings = set()
+                for node in nodes:
+                    strings |= set(islice(enumerated_strings, node.start, node.end + 1))
+
+                # Restore original order of the strings
+                strings = map(itemgetter(1), sorted(strings, key=itemgetter(0)))
+                return strings
---
+++ tests/test_bisect.py
@@ -0,0 +1,44 @@
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+from csv_bisect_gui.bisect_tool import Node
+
+
+@given(st.lists(st.text()))
+def test_node(data):
+    node = Node(data)
+    assert node.size == len(data)
+    assert node.start == 0 and node.end == len(data) - 1
+    if len(data) == 0:
+        assert node.tree_text == "[] (0 strings)"
+        assert node.column_text == "<empty>"
+    elif len(data) == 1:
+        assert node.tree_text == f"[{node.start} : {node.end}] ({node.end - node.start + 1} string)"
+        assert node.column_text == f"{data[0]!r}"
+    elif len(data) == 2:
+        assert node.tree_text == f"[{node.start} : {node.end}] ({node.end - node.start + 1} strings)"
+        assert node.column_text == ",".join(map(repr, data))
+    else:
+        assert node.tree_text == f"[{node.start} : {node.end}] ({node.end - node.start + 1} strings)"
+        assert node.column_text == f"{data[node.start]!r} ... {data[node.end]!r}"
+
+
+@given(st.lists(st.text()))
+def test_split(data: list):
+    node = Node(data)
+
+    if len(data) < 2:
+        with pytest.raises(AssertionError):
+            node.split()
+    else:
+        child_1, child_2 = node.split()
+
+        if len(data) % 2 == 0:
+            assert child_1.size == child_2.size == len(data) // 2
+        else:
+            assert child_1.size == len(data) // 2 + 1
+            assert child_2.size == len(data) // 2
+
+        assert child_1.size + child_2.size == len(data)
+        assert list(child_1.items) + list(child_2.items) == data
