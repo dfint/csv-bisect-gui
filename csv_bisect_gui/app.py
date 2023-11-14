20459376774c5520c6cfc76b5f5388698ffe766c
insolor <insolor@gmail.com>
2023-11-14 21:35:01 +0300

    Process selected file

--- csv_bisect_gui/app.py
+++ csv_bisect_gui/app.py
@@ -1,16 +1,40 @@
+import platform
 import tkinter as tk
+from pathlib import Path
 from tkinter import filedialog, messagebox
 
 from csv_bisect_gui.bisect_tool import BisectTool
 
+is_windows = platform.system() == "Windows"
+
 
 class Window(tk.Tk):
+    bisect_tool: BisectTool
+
+    file_types: list[tuple[str, str]]
+
+    executable_path: Path | None
+    csv_path: Path | None
+
     def __init__(self):
         super().__init__()
         self.config(menu=self.create_main_menu())
 
         self.bisect_tool = BisectTool(self)
         self.bisect_tool.pack(fill=tk.BOTH, expand=True)
+
+        self.init_file_types()
+
+    def init_file_types(self):
+        self.file_types = [
+            ("exe files", "*.exe"),
+            ("dwarfort", "dwarfort"),
+            ("csv files", "*.csv"),
+            ("All files", "*.*"),
+        ]
+
+        if is_windows:
+            self.file_types.remove(("dwarfort", "dwarfort"))
 
     def create_main_menu(self):
         file_menu = tk.Menu(tearoff=False)
@@ -25,16 +49,22 @@ class Window(tk.Tk):
     def select_file(self):
         file_path = filedialog.askopenfilename(
             title="Select an executable or a csv file",
-            filetypes = (
-                ("exe files", "*.exe"),
-                ("dwarfort", "dwarfort"),
-                ("csv files", "*.csv"),
-                ("All files", "*.*"),
-            ),
+            filetypes=self.file_types,
         )
 
         if not file_path:
             return
+
+        file_path = Path(file_path)
+        if file_path.suffix == ".csv":
+            self.executable_path = None
+            self.csv_path = file_path
+        elif file_path.suffix == ".exe" or file_path.name == "dwarfort":
+            self.executable_path = file_path
+            self.csv_path = file_path.parent / "dfint_data" / "dfint_dictionary.csv"
+        else:
+            self.executable_path = None
+            self.csv_path = None
 
         messagebox.showinfo(title="Selected file", message=file_path)
 
